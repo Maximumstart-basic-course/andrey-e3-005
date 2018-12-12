fa7e6551ebb0d881f978bd855d300fa4e58beec3
8safonov <8safonov@gmail.com>
2018-12-12 15:02:20 +0200

    Підготував 5 домашку без answers

---
+++ app/pages/Todo.js
@@ -0,0 +1,38 @@
+const todo = {
+    title: [],
+    description: [],
+    dueDate: [],
+    addTask(title, desc, dueDate) {
+        todo.title.push(title);
+        todo.description.push(desc);
+        todo.dueDate.push(dueDate);
+        console.log(`Добавлен таск: { title: '${title}', description: '${desc}', dueDate: '${dueDate}'.}`);
+    },
+    getTasks(date) {
+        todo.dueDate.filter((el, i, arr) => {
+            if (el === date) {
+            console.log(`${el}, ${todo.description[i]}, ${todo.title[i]}`);
+        } else {
+            console.log(`Тасков на '${date}' нет.`)
+        }
+        });
+    },
+    deleteTask(title) {
+        todo.title.find((el, i) =>{
+            if (el === title) {
+                todo.title.splice(i, 1);
+                console.log(`Таск удален.`);
+            } else {
+                console.log(`Таск '${title}' не найден.`)
+            }
+        }
+
+    )},
+
+};
+
+const user = [{name: 'Bruce', surname: 'Doe'}];
+
+console.log(user[0].name);
+
+user.push('age: 41');
